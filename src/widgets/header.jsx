6f6ea785364053b8eec5dad36e79ac7ce132e07d
Khalikov <MMMAAALLLIII@vk.ru>
2024-04-15 02:22:46 +0300

    Внесены правки в хедер, закончена страница main.

--- src/widgets/header.jsx
+++ src/widgets/header.jsx
@@ -1,37 +1,50 @@
-import React, { useState } from 'react';
-import {BrowserRouter as Router,Routes, Route, Link} from 'react-router-dom';
-import LogoBash from "../img/logo-bash.png"
-import LogoIpa from "../img/logo-ipa.png"
-import './widgets.scss';
-import {MenuButton} from "../widgets/menuButton";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LogoBash from "../img/logo-bash.png";
+import LogoIpa from "../img/ipa--bro.png";
+import "./widgets.scss";
+import { MenuButton } from "../widgets/menuButton";
 
 export default function Header() {
-    const [isOpen, setOpen] = useState();
+  const [isOpen, setOpen] = useState();
 
   return (
     <div className="header">
+      <div className="header-logos">
         <div className="header__logo1">
-            <img src={LogoBash}/>
+          <img src={LogoBash} />
         </div>
         <div className="header__logo2">
-            <img src={LogoIpa}/>
+          <img src={LogoIpa} />
         </div>
-        <div className={`header__nav ${isOpen ? "active" : ""}`}>
-            <ul className="header__nav-list">
-                <li className="header__nav-item"><Link to="/">Главная</Link></li>
-                <li className="header__nav-item"><Link to="/news">Новости</Link></li>
-                <li className="header__nav-item"><Link to="/members">Участники</Link></li>
-                <li className="header__nav-item"><Link to="/join">Вступить</Link></li>
-                <li className="header__nav-item"><Link to="/donations">Пожертвовать</Link></li>
-                <li className="header__nav-item"><Link to="https://mpa71.ru/">Академия</Link></li>
-                <li className="header__nav-item"><Link to="/contacts">Контактная информация</Link></li>
-                
-            </ul>
-        </div>
-        <MenuButton isActive={isOpen} onClick={()=> setOpen(!isOpen)}/>
+      </div>
+      <div className={`header__nav ${isOpen ? "active" : ""}`}>
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <Link to="/">Главная</Link>
+          </li>
+          <li className="header__nav-item">
+            <Link to="/news">Новости</Link>
+          </li>
+          <li className="header__nav-item">
+            <Link to="/members">Участники</Link>
+          </li>
+          <li className="header__nav-item">
+            <Link to="/join">Вступить</Link>
+          </li>
+          <li className="header__nav-item">
+            <Link to="/donations">Пожертвовать</Link>
+          </li>
+          <li className="header__nav-item">
+            <Link to="https://mpa71.ru/">Академия</Link>
+          </li>
+          <li className="header__nav-item">
+            <Link to="/contacts">Контактная информация</Link>
+          </li>
+        </ul>
+      </div>
+      <MenuButton isActive={isOpen} onClick={() => setOpen(!isOpen)} />
     </div>
-
-
 
     // <div className="header">
     //     <div className="container">
@@ -57,6 +70,5 @@ export default function Header() {
     //         </div>
     //     </div>
     // </div>
-
-  )
+  );
 }
